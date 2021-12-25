dd3539ee4ab6ed7745431757248b8221d29ffcda
GustavoHCpunk <gustavo@henrimichel.com.br>
2021-12-24 21:16:09 -0300

    Exercitando e marcando linhas

--- Lista1.md
+++ Lista1.md
@@ -7,15 +7,21 @@ Faça um Programa que peça dois números e imprima a soma. -RESOLVIDO
 Faça um Programa que peça as 4 notas bimestrais e mostre a média. - RESOLVIDO
 Faça um Programa que converta metros para centímetros. - RESOLVIDO
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. - RESOLVIDO
-Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-C = 5 * ((F-32) / 9).
-Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. -RESOLVIDO
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês. - RESOLVIDO
+
+Faça um Programa que peça a temperatura em graus Fahrenheit,
+transforme e mostre a temperatura em graus Celsius.
+C = 5 * ((F-32) / 9). -Resolvida
+
+Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. - RESOLVIDO
+
 Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
-o produto do dobro do primeiro com metade do segundo .
-a soma do triplo do primeiro com o terceiro.
-o terceiro elevado ao cubo.
+a) O produto do dobro do primeiro com metade do segundo .
+b) A soma do triplo do primeiro com o terceiro.
+c) O terceiro elevado ao cubo. - RESOLVIDA
+
 Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
 Para homens: (72.7*h) - 58
--- resolucao/lista1/Lista1.java
+++ resolucao/lista1/Lista1.java
@@ -55,7 +55,7 @@ public class Lista1{
         float pi = 3.14f;
         float area = pi * potenciaResumida;
         System.out.println(" A Area do circulo é de "+area);
-        */
+    
         //Faça um Programa que calcule a área de um quadrado, 
         //em seguida mostre o dobro desta área para o usuário.
 
@@ -66,6 +66,64 @@ public class Lista1{
 
         System.out.println("A Área do quadrado é "+areaQuad);
         System.out.println("O dobro da Área do quadro é "+areaQuad*2);
+        
+        //Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+        //Calcule e mostre o total do seu salário no referido mês.
+        System.out.println("Quanto você ganha por hora? ");
+        float horaValor = entrada.nextFloat();
+        System.out.println("Quantas horas você trabalha por semana? ");
+        int horasTrabalhadas = entrada.nextInt();
+        int valorSemana = (int)horaValor * horasTrabalhadas;
+        int valorPmes = valorSemana*4;
+        System.out.println("Você ganha "+valorSemana+" por semana e "+valorPmes+" por mês.");
+        
+        //Faça um Programa que peça a temperatura em graus Fahrenheit,
+        //transforme e mostre a temperatura em graus Celsius.
+        //C = 5 * ((F-32) / 9).
+        System.out.println("informe a temperatura em Fahrenheit");
+        int fahrenheit = entrada.nextInt();
+        int celsius = ((fahrenheit-32)*5)/9;
+        double celsiusflo = (double)celsius;
+        System.out.println("A temperatura de "+fahrenheit+" é igual a "+celsiusflo+" em celsuis.");
+        //Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+        System.out.println("informe a temperatura em celsius");
+        double cels = entrada.nextDouble();
+        double fahren = ((cels * 9)/5)+32;
+        System.out.println("A temperatura de "+cels+"em celsius é igual a "+fahren+" em fahrenheit.");
+        */
+
+        /*Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+
+            a) O produto do dobro do primeiro com metade do segundo.
+
+            formula;
+            2x + number1 = number / 2
+            x = produto. 
+
+            b) A soma do triplo do primeiro com o terceiro.
+
+            formula:
+            3* number1 + real = resultado 
+            
+            c) O terceiro elevado ao cubo.
+            real ** 3
+        */
+        System.out.println("Digite o primeiro número inteiro ");
+        int number1 = entrada.nextInt();
+        System.out.println("Digite o segundo número inteiro");
+        int number2 = entrada.nextInt();
+        System.out.println("Digite o numero real ");
+        double  real = entrada.nextDouble();
+        int metade = number2/2;
+        int produto = number1*2*metade;
+        System.out.println("O primeiro numero foi "+number1);
+        System.out.println("O segundo numero foi "+number2);
+        System.out.println("O terceiro numero foi "+real);
+        System.out.println("a) O produto do dobro do primeiro com metade do segundo. Resposta "+produto);
+        int somaB = (number1 * 3)+ (int)real;
+        System.out.println("b) A soma do triplo do primeiro com o terceiro. Resposta "+somaB);
+        double triplo = Math.pow(real, 3);
+        System.out.println("c) O terceiro elevado ao cubo. Resposta "+triplo);
         entrada.close();
     }
 
