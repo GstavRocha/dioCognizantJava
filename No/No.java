1b99780daa5dbd049782acb7133b0de283faa0ba
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-02-04 06:13:21 -0300

    Testando filas

---
+++ No/No.java
@@ -0,0 +1,38 @@
+public class No{
+    private Object objeto;
+    private No refNo;
+
+
+    //construtor.
+
+    public No(){
+
+    }
+    public No (Object objeto){
+        this.objeto = objeto;
+        this.refNo = null;
+    }
+
+    //Gets e Sets
+
+    public void setObjeto(Object objeto){
+        this.objeto = objeto;
+    }
+
+    public Object getObjeto(){
+        return objeto;
+    }
+
+    public void setRefNo(No refNo){
+        this.refNo = refNo;
+    }
+
+    public No getNo(){
+        return refNo;
+    }
+
+    @Override
+    public String toString(){
+        return "No{"+"objeto="+objeto+"}";
+    }
+}
